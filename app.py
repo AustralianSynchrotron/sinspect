fd397e75717fbc4321fa16bfe4a248ecc78ccca3
Gary Ruben <gary.ruben@gmail.com>
2012-11-29 12:12:23 +1100

    code comments

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from traits.api import Str, Bool, Enum, List, HasTraits, Instance, Button, \
     HTML, on_trait_change
 from traitsui.api import View, Group, HGroup, VGroup, HSplit, \
     Item, UItem, TreeEditor, TreeNode
-from pyface.api import ImageResource
+from pyface.api import ImageResource, DirectoryDialog, OK
 from fixes import fix_background_color
 from chaco.api import OverlayPlotContainer, Plot, ArrayPlotData, \
     add_default_axes, add_default_grids, create_line_plot, Legend, PlotAxis
@@ -22,15 +22,15 @@ APP_WIDTH = 800
 title = "SinSPECt"
 app_icon = os.path.join('resources','app_icon.ico')
 
-# Traited versions of SPECS xml file classes - used by TreeEditor widget
+# SPECSRegion, SPECSGroup and SpecsFile are Traited versions of SPECS xml file classes
+# that represent nodes in the TreeEditor widget
 
 class SPECSRegion(HasTraits):
-    '''
-    The traited SPECSRegion contains a specs.SPECSRegion object
+    ''' The traited SPECSRegion contains a specs.SPECSRegion object and
+    represents a Region node in the TreeEditor
     '''
     name   = Str('<unknown>')
-    title  = Str
-    region = Instance(specs.SPECSRegion)
+    region = Instance(specs.SPECSRegion)    # The reference to the contained region object
     selection = Instance('SelectorPanel')   # A string argument here allows a forward reference
 
     def __init__(self, name, region, **traits):
@@ -41,21 +41,36 @@ class SPECSRegion(HasTraits):
         # Traited SPECSRegion owner
         self.region.owner = self
         self.selection = SelectorPanel(self)
-        # Instantiation of the SelectorPanel creates self.selection.counts == False
-        # Setting it True now triggers a trait change event which is what we desire to turn the plot on
+        # Instantiation of the SelectorPanel creates it with self.selection.counts==False
+        # Setting self.selection.counts=True now triggers a trait change event which
+        # triggers creation of the related plot series
         self.selection.counts = True
+
+    def get_x_axis(self):
+        ''' Return x-axis data based on the scan_mode metadata '''
+        r = self.region
+        if r.scan_mode == 'FixedAnalyzerTransmission':
+            xs = r.binding_axis
+        elif r.scan_mode == 'ConstantFinalState':
+            xs = r.excitation_axis
+        else:
+            xs = r.kinetic_axis
+        return xs
 
 
 class SPECSGroup(HasTraits):
+    ''' A group node in the TreeEditor '''
     name = Str('<unknown>')
-    specs_regions = List(SPECSRegion)
+    specs_regions = List(SPECSRegion)   # container for the subordinate regions
 
 
 class SpecsFile(HasTraits):
+    ''' The file node in the TreeEditor '''
     name = Str('<unknown>')
-    specs_groups = List(SPECSGroup)
+    specs_groups = List(SPECSGroup)     # container for the subordinate groups
 
     def open(self, filename):
+        ''' Create all objects corresponding to the tree '''
         s = specs.SPECS(filename)
         self.name = filename
         for group in s.groups:
@@ -73,32 +88,69 @@ class TreePanel(HasTraits):
 #    # When a selection is made this gets a reference to the selected tree node instance
 #    node_selection = Instance(HasTraits)
 
-    # Button group above tree area
+    # Buttons in the widget group above the tree area
     bt_open_file = Button("Open file...")
     bt_export_file = Button("Export...")
 
     def _bt_open_file_changed(self):
+        ''' Event handler
+        Called when user clicks the Open file... button
+        '''
         file_path = get_file_from_dialog()
         if file_path is not None:
             self.most_recent_path = os.path.dirname(file_path)
             self.file_path = file_path
 
     def _file_path_changed(self, new):
-        """
+        ''' Trait event handler
         When the file dialog box is closed with a file selection, open that file
-        """
+        '''
         self.name = self.file_path
         self.specs_file = SpecsFile().open(self.file_path)
 
-    def bt_export_file_changed(self):
-#        file_path = get_file_from_dialog()
-#        if file_path is not None:
-#            self.most_recent_path = os.path.dirname(file_path)
-#            self.file_path = file_path
-#        dlg = FileDialog(title='Choose file', action='save', wildcard=txt_wildcard)
-#        if dlg.open() == OK:
-#            return dlg.paths[0]
-        pass
+    def _bt_export_file_changed(self):
+        ''' Event handler
+        Called when the user clicks the Export... button
+        '''
+        dlg = DirectoryDialog(title='Save results', default_path=self.most_recent_path)
+        if dlg.open() == OK:
+            self.most_recent_path = dlg.path
+
+            # Export all regions in all groups
+            for g in self.specs_file.specs_groups:
+                for r in g.specs_regions:
+                    # make file region.name+'.xy' in directory g.name
+                    print g.name, r.name
+                    # Column data contains the following in left-to-right order:
+                    # x-axis, counts, channel_counts_n and extended_channels_n
+
+                    # x-axis
+                    a = [r.get_x_axis()]
+                    any_checked = False
+
+                    # counts
+                    if r.selection.counts:
+                        a.append(r.region.counts)
+                        any_checked = True
+
+                    # channel_counts_n
+                    for key, val in sorted(r.selection.get_trait_states().iteritems()):
+                        if val and ('channel_counts_' in key):
+                            a.append(r.region.channel_counts[:,r.selection._get_name_num(key)-1])
+                            any_checked = True
+
+                    # extended_channels_n
+                    for key, val in sorted(r.selection.get_trait_states().iteritems()):
+                        if val and ('extended_channels_' in key):
+                            a.append(r.region.extended_channels[:,r.selection._get_name_num(key)-1])
+                            any_checked = True
+
+                    # Write it
+                    if any_checked:
+                        a = np.array(a).transpose()
+                        #TODO: what format and newline character are we using here?
+                        filename = os.path.join(dlg.path, g.name+'_'+r.name+'.xy')
+                        np.savetxt(filename, a)
 
     def _has_data(self):
         return self.file_path is not None
@@ -124,6 +176,7 @@ class TreePanel(HasTraits):
         all-on -> last-selection -> all-off -> all-on -> ...
         '''
         print 'dr', self.name, self.region.owner.name
+
 
     # View for objects that aren't edited
     no_view = View()
@@ -181,8 +234,7 @@ class TreePanel(HasTraits):
 
 
 class PlotPanel(HasTraits):
-    '''
-    Based largely on the Chaco Overlapping plots demo
+    ''' The Chaco plot area.
     '''
     plot_data = Instance(ArrayPlotData)
     plot = Instance(Plot)
@@ -195,6 +247,7 @@ class PlotPanel(HasTraits):
         self.plot.index_axis = PlotAxis(self.plot, orientation='bottom', title='Energy [eV]')
         self.plot.y_axis = PlotAxis(self.plot, orientation='left', title='Intensity [Counts]')
         self.first = True
+
 
     def add_plot(self, name, xs, ys, **lineplot_args):
         self.plot_data.set_data(name+'_xs', xs)
@@ -219,7 +272,7 @@ class PlotPanel(HasTraits):
         self.plot.request_redraw()
 
     def _setup_plot_tools(self, plot):
-        """Sets up the background, and several tools on a plot"""
+        ''' Sets up the background, and several tools on a plot '''
         # Make a white background with grids and axes
 #        plot.bgcolor = 'white'
         plot.bgcolor='transparent'
@@ -248,7 +301,7 @@ class PlotPanel(HasTraits):
         return plot.value_mapper, plot.index_mapper, legend
 
     def _setup_mapper(self, plot, value_mapper, index_mapper):
-        """Sets up a mapper for given plot"""
+        ''' Sets up a mapper for given plot '''
         plot.value_mapper = value_mapper
         value_mapper.range.add(plot.value)
         plot.index_mapper = index_mapper
@@ -272,19 +325,17 @@ class SelectorPanel(HasTraits):
     '''
     A panel of checkboxes reflecting the channels within the specs region used for
     toggling plot visibility and flagging for export.
-    What is really needed here is a dynamic view that creates a checkbox for each
-    Bool trait in the region object. However, dynamic views seem extremely tricky, so
-    I'll just reflect the state of the region object's traits in the checkboxes here.
+    A "better" way to implememnt this would be to create a "dynamic view" that has a
+    checkbox for each Bool trait in the region object. However, dynamic views seem
+    extremely tricky, so instead I just reflect the state of the region object's traits
+    in the checkboxes here.
+    See TraitsUI documentation Example 5b
+    http://docs.enthought.com/traitsui/traitsui_user_manual/advanced_view.html
+    Instead of just defining a default view here, I build it using
+    def default_traits_view(self):
     '''
     name = Str('<unknown>')
     region = Instance(SPECSRegion)
-
-    '''
-    GR - see TraitsUI documentation Example 5b
-    http://docs.enthought.com/traitsui/traitsui_user_manual/advanced_view.html
-    Instead of just defining a default view here, build it using
-    def default_traits_view(self):
-    '''
 
     def __init__(self, region=None, **traits):
         super(SelectorPanel, self).__init__(**traits)   # HasTraits.__init__(self, **traits)
@@ -292,38 +343,66 @@ class SelectorPanel(HasTraits):
             return
         self.region = region
 
-        # create global counts indicator
+        # create a trait for the counts checkbox
         self.add_trait('counts', Bool)
 
-        # create channel indicators
+        # create traits for each channel_counts_n checkbox
         channel_counts_len = region.region.channel_counts.shape[1]
         for i in range(channel_counts_len):
             self.add_trait('channel_counts_{}'.format(i+1), Bool)
         # use self._instance_traits() to list these traits
 
-        # create extended channel indicators
+        # create traits for each extended_channels_n checkbox
         extended_channels_len = region.region.extended_channels.shape[1]
         for i in range(extended_channels_len):
             self.add_trait('extended_channels_{}'.format(i+1), Bool)
+        # Now we've created all the Bool/checkbox traits default_traits_view() can
+        # create a view for them.
+
+    def default_traits_view(self):
+        '''
+        Called to create the selection view to be shown in the selection panel.
+        This is also the default View called when the GUI is first initialised with a
+        "None" SelectorPanel.
+        https://mail.enthought.com/pipermail/enthought-dev/2012-May/031008.html
+        '''
+        trait_dict = self._instance_traits()
+        items = []
+        if 'counts' in trait_dict:
+            group1 = HGroup()
+            group1.content = []
+            group1.content.append(Item('counts', label='Counts'))
+            # channel_counts_x group
+            group = HGroup()
+            group.content = [Item(name, label=name.split('_')[-1])
+                            for name in sorted(trait_dict) if 'channel_counts_' in name]
+            group.show_border = True
+            group.label = 'Channel Counts'
+            group1.content.append(group)
+            # extended_channels_x group
+            group = HGroup()
+            group.content = [Item(name, label=name.split('_')[-1])
+                            for name in sorted(trait_dict) if 'extended_channels_' in name]
+            group.show_border = True
+            group.label = 'Extended Channels'
+            group1.content.append(group)
+            items.append(group1)
+        return View(*items)
 
     def _counts_changed(self, trait, old, new):
-#        print self.region.name, trait, old, new
+        ''' Trait event handler
+        The counts checkbox was toggled
+        '''
         if new:
             self._add_plot(self.region.name, trait)
         else:
             self._remove_plot(self.region.name, trait)
 
-    @on_trait_change('channel_counts_+')
+    @on_trait_change('channel_counts_+, extended_channels_+')
     def _channel_counts_x_changed(self, container, trait, new):
-#        print self.region.name, container, trait, new
-        if new:
-            self._add_plot(self.region.name, trait, self._get_name_num(trait))
-        else:
-            self._remove_plot(self.region.name, trait)
-
-    @on_trait_change('extended_channels_+')
-    def _extended_channels_changed(self, container, trait, new):
-#        print self.region.name, container, trait, new
+        ''' Trait event handler
+        A channel_counts_n or extended_channels_n checkbox was toggled
+        '''
         if new:
             self._add_plot(self.region.name, trait, self._get_name_num(trait))
         else:
@@ -336,34 +415,21 @@ class SelectorPanel(HasTraits):
         return '{}_{}'.format(region_name, series_name)
 
     def _get_name_body(self, series_name):
-        ''' Get first part of name.
+        ''' Get first part of name
+        e.g. _get_name_body('foo_bar_baz') returns foo_bar
         '''
         return '_'.join(series_name.split('_')[:2])
 
     def _get_name_num(self, series_name):
         ''' Get last part of name.
+        e.g. _get_name_num('foo_bar_baz') returns baz
         '''
         return int(series_name.split('_')[-1])
 
-#    def _get_y_axis(self, region, index=None, extended=False):
-#        r = region.region
-#        if index is not None:
-#            if extended:
-#            else:
-    def _get_x_axis(self, region):
-        r = region.region
-        if r.scan_mode == 'FixedAnalyzerTransmission':
-            xs = r.binding_axis
-        elif r.scan_mode == 'ConstantFinalState':
-            xs = r.excitation_axis
-        else:
-            xs = r.kinetic_axis
-        return xs
-
     def _add_plot(self, region_name, series_name, column=None):
-        ''' Call plot widget to create it. '''
+        ''' Adds a plot to the chaco plot widget. '''
         name = self._name_plot(region_name, series_name)
-        xs = self._get_x_axis(self.region)
+        xs = self.region.get_x_axis()
         if series_name == 'counts':
             ys = self.region.region.__getattribute__(series_name)
         else:
@@ -381,40 +447,18 @@ class SelectorPanel(HasTraits):
         plot_panel.add_plot(name, xs, ys, **line_attributes)
 
     def _remove_plot(self, region_name, series_name):
-        '''
-        Call plot widget to remove it and delete the reference here.
-        '''
+        ''' Call plot widget to remove it and delete the reference here. '''
         name = self._name_plot(region_name, series_name)
         plot_panel.remove_plot(name)
 
-    def default_traits_view(self):
+    def get_trait_states(self):
+        ''' Return a dictionary of trait_name:value entries associated with this
+        selector panel.
         '''
-        Called to create the selection view to be shown in the selection panel.
-        This is also the default View called when the GUI is first initialised with a
-        "None" SelectorPanel.
-        https://mail.enthought.com/pipermail/enthought-dev/2012-May/031008.html
-        '''
-        trait_dict = self._instance_traits()
-#        items = [Item(name) for name in sorted(trait_dict) if name is not 'trait_added']
-        items = []
-        if 'counts' in trait_dict:
-            group1 = HGroup()
-            group1.content = []
-            group1.content.extend([Item('counts', label='Counts')])
-            # channel_counts_x group
-            group = HGroup()
-            group.content = [Item(name, label=name.split('_')[-1]) for name in sorted(trait_dict) if 'channel_counts_' in name]
-            group.show_border = True
-            group.label = 'Channel Counts'
-            group1.content.append(group)
-            # extended_channels_x group
-            group = HGroup()
-            group.content = [Item(name, label=name.split('_')[-1]) for name in sorted(trait_dict) if 'extended_channels_' in name]
-            group.show_border = True
-            group.label = 'Extended Channels'
-            group1.content.append(group)
-            items.append(group1)
-        return View(*items)
+        # I don't know how to evaluate the trait values directly from _instance_traits()
+        # so I use it to get the names then use __getattribute__() to evaluate them.
+        return dict([(i, self.__getattribute__(i))
+                    for i in self._instance_traits().keys() if i is not 'trait_added'])
 
 
 class MainApp(HasTraits):
