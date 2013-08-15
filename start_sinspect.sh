565250c1dd14ec3217a482907fef0a9bdb1dc02c
Gary Ruben <gary.ruben@gmail.com>
2013-08-15 17:28:00 +1000

    Fix for MacOSX

--- start_sinspect.sh
+++ start_sinspect.sh
@@ -1,10 +1,3 @@
 #!/bin/bash                                                                     
-if [ `uname` == 'Darwin' ]
-then
-    if [ -f ~/.bash_profile ]
-    then
-        source ~/.bash_profile
-    fi
-fi
 echo "Starting SinSPECt..."
 python app.py
